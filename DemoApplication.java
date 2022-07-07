aaee7dc01ba999e7ee93676af1b80f082a873a3b
NovikovMaksym <79098815+JakeDogee@users.noreply.github.com>
2022-07-07 18:05:30 +0300

    Update DemoApplication.java

--- DemoApplication.java
+++ DemoApplication.java
@@ -10,11 +10,5 @@ public class DemoApplication {
     public static void main(String[] args) {
         ApplicationContext context = new AnnotationConfigApplicationContext(BeansConfig.class);
         context.getBean(Pet.class).printPet();
-
-        Cheetah cheetahByName = (Cheetah) context.getBean("cheetah1");
-        System.out.println(cheetahByName);
-
-        Cheetah cheetahByType = context.getBean(Cheetah.class);
-        System.out.println(cheetahByType);
     }
 }
